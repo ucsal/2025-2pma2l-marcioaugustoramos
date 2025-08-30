3c2790dd4ccf3a233e9a2ef155a814d30ec7372c
marcioaugustoramos <marcioaugusto.filho@ucsal.edu.br>
2025-08-30 18:37:37 -0300

    Resolução da atividade

--- src/main/java/br/com/mariojp/solid/lspaccounts/Account.java
+++ src/main/java/br/com/mariojp/solid/lspaccounts/Account.java
@@ -1,17 +1,20 @@
 package br.com.mariojp.solid.lspaccounts;
 
-public class Account {
+public abstract class Account {
     protected double balance;
 
     public void deposit(double amount){
         this.balance += amount;
     }
 
-    public void withdraw(double amount){
-        this.balance -= amount;
-    }
-
     public double getBalance(){
         return balance;
+    }
+
+    // cada subclasse decide se tem saque
+    public abstract boolean canWithdraw();
+    
+    public void withdraw(double amount){
+        this.balance -= amount;
     }
 }
